bd1bd18dd0c8246939983ac82526556287ebc8e1
Marat Oshmiansky <maratical@gmail.com>
2021-08-20 10:44:38 -0700

    incorporate theme into ChordsShow - needs work

--- src/views/ChordsShow.vue
+++ src/views/ChordsShow.vue
@@ -1,41 +1,40 @@
 <template>
   <div class="chords-show">
-    <div class="container">
-      <h2>Chord name: {{ chord.name }}</h2>
-      <img v-bind:src="chord.image" alt="chord.name" />
-      <!-- <p>Root: {{ chord.chord_root }}</p>
-      <p>Alteration: {{ chord.alteration }}</p>
-      <p>Quality: {{ chord.quality }}</p>
-      <p>Option: {{ chord.option }}</p> -->
-      <!-- <router-link v-bind:to="`/chords/${chord.id}/edit`"><button>Edit Chord</button></router-link> -->
-      <form v-on:submit.prevent="createUserChord(chord)">
-        <br />
-        <h3>Add User Chord to Catalog</h3>
-        <ul>
-          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-        </ul>
-        <!-- <div>
-          <p>Chord ID: {{ chord.id }}</p>
-        </div> -->
-        <div class="col-md-6 form-group">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Enter catalog name"
-            required
-            v-model="newUserChordParams.catalog"
-          />
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <br />
+            <img v-bind:src="chord.image" class="img-fluid" alt="chord.name" />
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <br />
+            <br />
+            <h3>Chord name: {{ chord.name }}</h3>
+            <br />
+            <form v-on:submit.prevent="createUserChord(chord)">
+              <h2>Add User Chord to Catalog</h2>
+              <ul>
+                <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+              </ul>
+              <div class="col-md-6 form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter catalog name"
+                  required
+                  v-model="newUserChordParams.catalog"
+                />
+              </div>
+              <br />
+              <input type="submit" value="Submit" />
+            </form>
+            <br />
+            <router-link to="/chords">Back to all chords</router-link>
+          </div>
         </div>
-        <!-- <div>
-          <label>Catalog:</label>
-          <input type="text" v-model="newUserChordParams.catalog" />
-        </div> -->
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-      <br />
-      <router-link to="/chords">Back to all chords</router-link>
-    </div>
+      </div>
+    </section>
   </div>
 </template>
 
