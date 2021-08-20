604316d8194fd3945965b7612e18f0cc494ed046
Marat Oshmiansky <maratical@gmail.com>
2021-08-20 13:23:16 -0700

    incorporated theme into Home - need pic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,22 @@
 <template>
   <div class="home">
-    <br />
-    <br />
-    <br />
-    <h1>{{ message }}</h1>
-    <h2>{{ heading }}</h2>
-    <!-- <div v-for="chord in chords" v-bind:key="chord.id">
-      <h2>Chord: {{ chord.name }}</h2>
-      <img v-bind:src="chord.image" alt="chord.name" />
-    </div> -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <!-- <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="" />
+          </div> -->
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <br />
+            <h3>Welcome to Chorduroy - A Guitar Chord Finder App</h3>
+            <p class="fst-italic">Click on Chords in the menu to go to Chord Library</p>
+            <p class="fst-italic">Click on User Chords in the menu to go to your User Chords</p>
+            <!-- <p>(More text can go here...)</p> -->
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <style></style>
-<script>
-// import axios from "axios";
-export default {
-  data: function () {
-    return {
-      message: "Welcome to Chorduroy - A Guitar Chord Finder App",
-      heading: "Click on Chords in the menu to go to Chord Library",
-      // chords: [],
-    };
-  },
-  // created: function () {
-  //   this.indexChords();
-  // },
-  // methods: {
-  //   indexChords: function () {
-  //     axios.get("/chords").then((response) => {
-  //       this.chords = response.data;
-  //       console.log("All chords:", this.chords);
-  //     });
-  //   },
-  // },
-};
-</script>
+<script></script>
---
+++ src/views/Home_OLD.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <br />
+    <br />
+    <br />
+    <h1>{{ message }}</h1>
+    <h2>{{ heading }}</h2>
+    <!-- <div v-for="chord in chords" v-bind:key="chord.id">
+      <h2>Chord: {{ chord.name }}</h2>
+      <img v-bind:src="chord.image" alt="chord.name" />
+    </div> -->
+  </div>
+</template>
+<style></style>
+<script>
+// import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Chorduroy - A Guitar Chord Finder App",
+      heading: "Click on Chords in the menu to go to Chord Library",
+      // chords: [],
+    };
+  },
+  // created: function () {
+  //   this.indexChords();
+  // },
+  // methods: {
+  //   indexChords: function () {
+  //     axios.get("/chords").then((response) => {
+  //       this.chords = response.data;
+  //       console.log("All chords:", this.chords);
+  //     });
+  //   },
+  // },
+};
+</script>
